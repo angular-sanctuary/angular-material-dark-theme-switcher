29a4bef79e472c7f64f80a81fb077ce5dd2e1599
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-15 16:59:52 +0200

    feat: add dark theme switcher

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component, computed, inject, Renderer2, signal} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
@@ -10,6 +10,20 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class NavigationComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  private renderer = inject(Renderer2);
+
+  isDarkModeEnabled = signal(false);
+  darkModeIcon = computed(() => this.isDarkModeEnabled() ? 'dark_mode' : 'light_mode');
+
+  toggleTheme(): void {
+    if (this.isDarkModeEnabled()) {
+      this.renderer.removeClass(document.body, 'darkMode');
+    } else {
+      this.renderer.addClass(document.body, 'darkMode');
+    }
+
+    this.isDarkModeEnabled.update(isEnabled => !isEnabled);
+  }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
